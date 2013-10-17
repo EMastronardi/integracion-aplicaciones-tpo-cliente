b195b6aa45af4db13ef1c8d2b17a6fb46c8570af
emastronardi@gmail.com <emastronardi@gmail.com@81d44ce1-da5a-e293-f867-ae022b3cbc51>
2013-10-17 02:30:30 +0000

    Login modificado.

---
+++ trunk/TpoIA-Cliente/WebContent/Login.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Login - Despacho</title>
+</head>
+<body>
+<% String cadena = ""; 
+if(request.getParameter("usuario") != null){
+	cadena = "Usuario invalido!!!";
+	} 
+%>
+<style>
+*{
+	font-family: Verdana, Arial, Sans-Serif"
+}
+</style>
+<h1 style="color: #9FCF21; font-family: Verdana, Arial, Sans-Serif">M&oacute;dulo Despacho</h1>
+
+	<div class="content" style="border:1px dotted grey;display: inline;">
+		<h2>Login</h2>
+	<form action="Controlador?action=validarLogin" method="post" style="display:inline;">
+		<table >
+			<tr>
+				<td><span style="line-height: normal; font-family: Verdana, Arial, Sans-Serif; font-weight: bold">Usuario</span></td>
+				<td><input type="text" name="usuario"
+					style="cursor: text; height: auto; width: 138px; font-weight: normal"></td>
+			</tr>
+			<tr>
+				<td><span style="font-family: Verdana, Arial, Sans-Serif; font-weight: bold">Contraseña</span></td>
+				<td><input type="password" name="contraseña" style="width: 138px;"></td>
+			</tr>
+		</table>
+		<input type="submit" value="Aceptar" style=" font-style: normal; font-family: Verdana, Arial, Sans-Serif" />
+		<span><%= cadena %></span>
+		<br />
+		<a href="#">Nuevo Usuario</a>
+	</form>
+	</div>
+</body>
+</html>
---
+++ trunk/TpoIA-Cliente/src/servlets/Controlador.java
@@ -0,0 +1,66 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controlador
+ */
+@WebServlet("/Controlador")
+public class Controlador extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static Controlador instancia;
+
+	public Controlador() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		String jspPage = "/Login.jsp";
+
+		if ((action == null) || action.length() < 1) {
+			action = "default";
+		}
+		if ("default".equals(action)) {
+			jspPage = "/Login.jsp";
+		} else if ("salir".equals(action)) {
+			jspPage = "/Login.jsp";
+		}
+		dispatch(jspPage, request, response);
+	}
+
+	protected void dispatch(String jsp, HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		if (jsp != null) {
+			/*
+			 * Envía el control al JSP que pasamos como parámetro, y con los
+			 * request / response cargados con los parámetros
+			 */
+			RequestDispatcher rd = request.getRequestDispatcher(jsp);
+			rd.forward(request, response);
+		}
+	}
+
+}
