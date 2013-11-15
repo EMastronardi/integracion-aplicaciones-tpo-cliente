da92b96c0204e1e7f7f124f9ea937f274b2695f4
d.a.zanotti@gmail.com <d.a.zanotti@gmail.com@81d44ce1-da5a-e293-f867-ae022b3cbc51>
2013-11-15 17:54:18 +0000

    Clases para servicio rest

---
+++ trunk/TpoIA-Cliente/src/published/JaxRsActivator.java
@@ -0,0 +1,10 @@
+package published;
+
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/rest")
+public class JaxRsActivator extends Application{
+
+}
