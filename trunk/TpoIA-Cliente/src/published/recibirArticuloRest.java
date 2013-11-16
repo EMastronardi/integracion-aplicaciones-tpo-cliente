5fe848f0e46ebf4a8830fc80ba2257003ad31708
d.a.zanotti@gmail.com <d.a.zanotti@gmail.com@81d44ce1-da5a-e293-f867-ae022b3cbc51>
2013-11-16 19:34:29 +0000

    Servicio Rest terminado

--- trunk/TpoIA-Cliente/src/published/recibirArticuloRest.java
+++ trunk/TpoIA-Cliente/src/published/recibirArticuloRest.java
@@ -1,35 +1,21 @@
 package published;
-
-
-import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import model.BusinessDelegate;
 import xml.RespuestaXML;
-import integration.FacadeRemote;
 
 @Path("/rest")
 public class recibirArticuloRest {
-	
-	//@EJB
-	//FacadeRemote facade;
-	
 	@POST
 	@Path("/despachador/recibirArticulos")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public RespuestaXML recibirArticulos(String jsonData){
-
-		System.out.println("Recibido: " + jsonData.toString());
-		
-		RespuestaXML respuesta = new RespuestaXML();
-		respuesta.setEstado("OK");
-		respuesta.setMensaje("HOLA");
-		return respuesta;
-		
+	public RespuestaXML recibirArticulos(String jsonData){	
+		return BusinessDelegate.getInstance().recibirArticulos(jsonData);		
 	}
 
-	}
+}
